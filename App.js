581328d3bcaee5e2102447d40d2a02e0ba57b66b
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-01-01 23:51:48 -0600

    input and button

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+    const [output, setOutput] = useState('EasyDoseIt')
+    return (
+        <View style={
+            styles.container
+        }>
+            <View style={
+                {
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    alignItems: 'center'
+                }
+            }>
+                <TextInput placeholder="strain name"
+                    style={
+                        {
+                            width: '80%',
+                            borderBottomColor: "black",
+                            borderBottomWidth: 1,
+                            paddingBottom: 10
+                        }
+                    }/>
+                <Button color={styles.button.color} title="add"/>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        padding: 50
+    },
+    button: {
+        color: '#017C6B'
+    }
 });
