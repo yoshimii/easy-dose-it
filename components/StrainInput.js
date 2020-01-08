5dc02f1030e926dbb33e7c7e1450455c943bf413
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2020-01-07 23:47:12 -0600

    add strain modal and clear form function

--- components/StrainInput.js
+++ components/StrainInput.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Modal } from 'react-native';
 import Strain from './Strain.js'
 
 const StrainInput = props => {
@@ -8,23 +8,30 @@ const StrainInput = props => {
     const handleStrain = enteredStrain => {
         setStrain(enteredStrain)
       }
+    
+      const addStrainHandler = _=> {
+          props.onAddStrain(strain);
+          setStrain('');
+      }
 
     return <>
-    <View style={
-        styles.inputContainer
-      }>
-          <TextInput placeholder="strain name" onChangeText={handleStrain} value={strain}
-              style={
-                  styles.strainInput
-              }/>
-          <Button color={
-                  styles.button.color
-              }
-              title="add"
-              onPress={props.onAddStrain.bind(this, strain)}
-              />
-      </View>
-
+    <Modal visible={props.visible} animationType="slide">
+        <View style={
+            styles.inputContainer
+        }>
+            <TextInput placeholder="strain name" onChangeText={handleStrain} value={strain}
+                style={
+                    styles.strainInput
+                }/>
+            <Button title='cancel' color="red" onPress={props.onCancel} />
+            <Button color={
+                    styles.button.color
+                }
+                title="add"
+                onPress={addStrainHandler}
+                />
+        </View>
+    </Modal>
       </>
 }
 
@@ -34,12 +41,14 @@ const styles = StyleSheet.create({
         width: '80%',
         borderBottomColor: "black",
         borderBottomWidth: 1,
-        paddingBottom: 10
+        paddingBottom: 10, 
+        marginBottom: 10
 
     },
     inputContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        flexDirection: 'column', //column is default flex direction in React Native
+        flex:1,//how much space flex items take, if only item 1 means the entire space, otherwise it will take the default space which is only the size of the items
+        justifyContent: 'center',
         alignItems: 'center'
     },
     button: {
